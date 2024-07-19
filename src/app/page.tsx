4763d8a36eb9213d6340f9f12a83a1f5c6be15c3
Max <allenmaxnichols@gmail.com>
2024-07-19 17:18:10 -0500

    add some schema and add styling to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Hero from "./ui/marketing/Hero";
+import Navigation from "./ui/marketing/Navigation";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Make this page the landing page kinda like yelp or verbo</p>
+    <main>
+      <Navigation />
+      <Hero />
     </main>
   );
 }
